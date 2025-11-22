763004b004ad2c2d72eac441c9bae3ad22b93914
CodeShotsAryan <aryanpformal@gmail.com>
2025-11-22 11:49:34 +0530

    Updated main and requirements

--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
-import importlib
-import pkgutil
+from .db import Base, engine
+from .routes import auth, admin_users, products, operations, ledger, dashboard
 
-app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
-@app.get('/')
+app = FastAPI(title="IMS Backend Modular")
+
+app.include_router(auth.router)
+app.include_router(admin_users.router)
+app.include_router(products.router)
+app.include_router(operations.router)
+app.include_router(ledger.router)
+app.include_router(dashboard.router)
+
+@app.get("/")
 def root():
-    return {"msg":"Odoo Hackathon 2025"}
+    return {"msg": "IMS API running"}
